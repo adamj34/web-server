b6a8ea54764a476e6eb799c6191fc50fbef49873
addar <trashci18@gmail.com>
2024-07-26 18:26:50 +0200

    added endpoint manager tests

--- build/tests_tests-b12d07c.cmake
+++ build/tests_tests-b12d07c.cmake
@@ -6,4 +6,6 @@ add_test( [==[Response set_header]==] /home/addar/web-server/build/tests [==[Res
 set_tests_properties( [==[Response set_header]==] PROPERTIES WORKING_DIRECTORY /home/addar/web-server/build)
 add_test( [==[Response to_string]==] /home/addar/web-server/build/tests [==[Response to_string]==]  )
 set_tests_properties( [==[Response to_string]==] PROPERTIES WORKING_DIRECTORY /home/addar/web-server/build)
-set( tests_TESTS [==[Request parsing]==] [==[Request to_string]==] [==[Response set_header]==] [==[Response to_string]==])
+add_test( [==[Endpoint manager add endpoint]==] /home/addar/web-server/build/tests [==[Endpoint manager add endpoint]==]  )
+set_tests_properties( [==[Endpoint manager add endpoint]==] PROPERTIES WORKING_DIRECTORY /home/addar/web-server/build)
+set( tests_TESTS [==[Request parsing]==] [==[Request to_string]==] [==[Response set_header]==] [==[Response to_string]==] [==[Endpoint manager add endpoint]==])
